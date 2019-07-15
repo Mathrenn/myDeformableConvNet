c583b75de315687954cfe1a10da079c96690e873
Mathrenn <carulli.matteo@gmail.com>
2019-07-15 12:27:45 +0200

    Interpolation operation and offset reduction

--- defConvNN.py
+++ defConvNN.py
@@ -8,7 +8,7 @@ import pdb
 
 # Deformable 1D Convolution
 class DeformableConv1D(Conv1D):
-    def __init__(self, filters, kernel_size,**kwargs):
+    def __init__(self, filters, kernel_size, **kwargs):
         super(DeformableConv1D, self).__init__(filters=filters, 
                                                kernel_size=kernel_size,
                                                kernel_initializer='zeros',
@@ -21,13 +21,14 @@ class DeformableConv1D(Conv1D):
     def call(self, x):
         # offset values are computed with a standard convolutional layer
         offset = super(DeformableConv1D, self).call(x)
-        assert(offset.shape[2] == x.shape[2])
+        #assert(offset.shape[2] == x.shape[2])
         # regular grid
-        R = regularGrid(self.kernel_size)
+        R = tf.constant(regularGrid(self.kernel_size[0]))
+        dpn = tf.math.reduce_mean(offset, [0,-1])
         # output feature map
-        y = linearInterpolation(x, offset, R)
+        y = linearInterpolation(x, R, dpn)
         y = tf.reshape(y, tf.shape(x))
-        return y
+        return y 
 
     def compute_output_shape(self, input_shape):
         return input_shape
@@ -54,14 +55,37 @@ def regularGrid(kernel_size):
     offset: (b, ts, c)
     R: (kernel_size)
 """
-def linearInterpolation(x, offset, R):
+def linearInterpolation(x, R, offset):
     # output map
-    y = tf.zeros_like(x)
+    y = tf.numpy_function(linInterOp, [x, R, offset], tf.float32)
+    return y
+    
+"""
+    linear interpolation operation
 
-    # for each location p0 in the output map y
-    ## for each location pn in the regular grid R
-    ### x(p) = sum(g(q, p), x(q)) over the input locations q
-    ### with p = p0 + pn + dpn and dpn offset location
+    for each location p0 in the output map y
+     for each location pn in the regular grid R
+      x(p) = sum(g(q, p), x(q)) over the input locations q
+       with p = p0 + pn + dpn and dpn offset location
+    
+    input:
+        x: input feature map
+        R: regular grid
+        dpn: offsets
+    output:
+        y: offset feature map
+"""
+def linInterOp(x, R, dpn):
+    y = np.zeros_like(x)
+    for p0 in range(y.shape[0]):
+        for pn in R:
+            # offset location
+            p = p0 + pn + dpn[pn]
+            glist = np.zeros_like(x)
+            for q in range(x.shape[0]):
+                glist[q] = G(q,p)
+            y[p0] = np.sum(x * glist)
+    return y
 
 """
     linear interpolation kernel
