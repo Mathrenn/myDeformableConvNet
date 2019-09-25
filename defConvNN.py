e8fab8aff607eb0560922683ebad97b37bb21f33
Mathrenn <carulli.matteo@gmail.com>
2019-09-25 10:58:02 +0200

    First working alternative, see the readme

--- defConvNN.py
+++ defConvNN.py
@@ -1,7 +1,10 @@
+#!pip install tensorflow-gpu==2.0.0-rc2
+
 # Following https://keras.io/layers/writing-your-own-keras-layers/
 # https://www.tensorflow.org/beta/guide/keras/custom_layers_and_models
 
 import tensorflow as tf
+import tensorflow.keras.backend as K
 from tensorflow.keras.layers import Layer, Conv1D
 from tensorflow.python.keras.utils import conv_utils
 from tensorflow.python.framework import tensor_shape
@@ -10,126 +13,79 @@ import numpy as np
 import pdb
 
 # Deformable 1D Convolution
-class DeformableConv1D(Conv1D):
-    def __init__(self, filters, kernel_size, offset, **kwargs):
-        super(DeformableConv1D, self).__init__(
-                    filters=filters, 
-                    kernel_size=kernel_size,
-                    **kwargs)
-
-        self.offshape = offset.shape
-        self.offset = tf.reshape(offset, [-1, 1])
-
-        self.R = tf.constant(
-                    regularGrid(kernel_size), 
-                    tf.float32)
-
+class DeformableConv1D(tf.keras.layers.Layer):
+    def __init__(self, filters, kernel_size, **kwargs):
+        super(DeformableConv1D, self).__init__()
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.R = tf.constant(self.regularGrid(self.kernel_size), tf.float32)
+        self.offconv = Conv1D(filters, kernel_size, trainable=False, **kwargs)
 
     def build(self, input_shape):
-        input_shape = tensor_shape.TensorShape(input_shape)
-        if self.data_format == 'channels_first':
-            channel_axis = 1
-        else:
-            channel_axis = -1
-        if input_shape.dims[channel_axis].value is None:
-            raise ValueError('The channel dimension of the inputs should be defined. Found `None`.')
-        input_dim = int(input_shape[channel_axis])
-        W_shape = self.kernel_size + (1,)
+        W_shape = (self.kernel_size, 1)
         self.W = self.add_weight(
             name='W',
             shape=W_shape,
-            initializer=self.kernel_initializer,
-            regularizer=self.kernel_regularizer,
-            constraint=self.kernel_constraint,
             trainable=True,
             dtype=self.dtype)
-
-        if self.padding == 'causal':
-            op_padding = 'valid'
-        else:
-            op_padding = self.padding
-        if not isinstance(op_padding, (list, tuple)):
-            op_padding = op_padding.upper()
-
-
-        self.bias = None
         self.built = True
 
     def call(self, x):
         # output feature map
-        y = linearInterpolation(x, self.R, self.offset, self.W)
-        y = tf.reshape(y, self.offshape)
-        return y
+        offset = self.offconv(x)
+        y = self.linearInterpolation(x, offset)
+        y = tf.reduce_sum(self.W * y, [0])
+        
+        #y = tf.reshape(y, [-1, offset.shape[1], offset.shape[2]])
+        y = tf.reshape(y, [-1, x.shape[1], x.shape[2]])
+        
+        return self.offconv(y)
 
-    def compute_output_shape(self, input_shape):
-        return super(DeformableConv1D, self).compute_output_shape(input_shape)
+    """
+       Regular grid
+       kernel_size: integer
+    """
+    def regularGrid(self, kernel_size):
+        R = np.zeros(kernel_size, dtype='int32')
+        j = -(np.floor(kernel_size/2))
+        for i in range(R.shape[0]):
+            R[i] = j
+            j += 1
+        return R
 
-"""
-    Regular grid
+    """
+        linear interpolation
+        x: (b, ts, c)
+        offset: (b, ts, c)
+        R: (kernel_size)
+    """
+    def linearInterpolation(self, x, offset):
+        Q = tf.where(tf.equal(K.flatten(x), K.flatten(x)))
+        Q = tf.cast(Q, tf.float32)
+        #Q = tf.range(0, x.shape[0]*x.shape[1]*x.shape[2], dtype=tf.float32)
 
-    kernel_size: integer
-"""
-def regularGrid(kernel_size):
-    R = np.zeros(kernel_size, dtype='int32')
-    j = -(np.floor(kernel_size/2))
+        R = tf.cast(self.R, tf.float32)
+        dpn = tf.reshape(offset, [-1, 1]) + R
+        dpn = tf.math.reduce_mean(dpn, [0])
 
-    for i in range(R.shape[0]):
-        R[i] = j
-        j += 1
+        dpn_list = tf.unstack(dpn)
+        ylist = []
 
-    return R
+        for pn in dpn_list:
+          G = self.g(Q, Q+pn)
+          ylist.append(G * K.flatten(x))
 
-"""
-    linear interpolation
+        return tf.stack(ylist)
 
-    x: (b, ts, c)
-    offset: (b, ts, c)
-    R: (kernel_size)
-"""
-def linearInterpolation(x, R, offset, W):
-    # output map
-    y = linInterOp(x, R, offset, W)
-    return y
+    """
+        linear interpolation kernel
+        q: input location
+        p: offset location
+    """
+    def g(self, q, p):
+        #pdb.set_trace()
+        g = tf.subtract(tf.squeeze(q), tf.squeeze(p))
+        g = tf.abs(g)
+        g = tf.subtract(1.0, g)
+        return tf.maximum(0.0, g)
 
-"""
-    linear interpolation operation
-    
-    input:
-        x: input feature map
-        R: regular grid
-        dpn: offsets
-    output:
-        y: offset feature map
-"""
-def linInterOp(x, R, dpn, W):
-    R = tf.cast(R, tf.float32)
-    off = dpn + R
-    off = tf.math.reduce_mean(off, [0])
-    x1d = tf.reshape(x, [-1])
-    xshape = x1d.shape
-    Q = tf.range(xshape[0])
-    Q = tf.cast(Q, tf.float32)
-    P = tf.range(tf.shape(tf.reshape(dpn, [-1]))[0])
-    P = tf.cast(P, tf.float32)
-
-    Q1 = tf.compat.v2.expand_dims(Q, [-1])
-    P1 = tf.compat.v2.expand_dims(P, [0])
-
-    y = g(Q1, P1)
-    y = y * tf.compat.v2.expand_dims(x1d, [-1])
-    y = tf.reduce_sum(y, [0])
-    y = y * W
-    y = tf.reduce_sum(y, [0])
-    return y
-
-"""
-    linear interpolation kernel
-    
-    q: input location
-    p: offset location
-"""
-def g(q, p):
-    g = q-p
-    g = tf.abs(g)
-    g = tf.subtract(1.0, g)
-    return tf.maximum(0.0, g)
--- imdb.py
+++ imdb.py
@@ -1,71 +1,70 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.datasets import imdb
+from __future__ import print_function
+
 from tensorflow.keras.preprocessing import sequence
-import pdb
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Dropout, Activation
+from tensorflow.keras.layers import Embedding, Input
+from tensorflow.keras.layers import Conv1D, GlobalMaxPooling1D
+from tensorflow.keras.datasets import imdb
+from tensorflow.python.keras.engine.base_layer import InputSpec
 
+# set parameters:
 max_features = 10000
-maxlen = 500
-batch_size = 64
+maxlen = 400
+batch_size = 32
+embedding_dims = 50
+filters = 250
+kernel_size = 3
+hidden_dims = 250
+epochs = 2
 
-print("Downloading dataset...")
-
+print('Loading data...')
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
 
-print("Dataset downloaded.")
-
-print("Pad sequences (sample x time)")
+print('Pad sequences (samples x time)')
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
 x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
 print('x_train shape:', x_train.shape)
 print('x_test shape:', x_test.shape)
 
-from tensorflow.keras.layers import Dense, Input, Embedding, Conv1D, GlobalMaxPooling1D, MaxPooling1D
-from tensorflow.keras import models
-from tensorflow.keras.optimizers import RMSprop
-from defConvNN import DeformableConv1D
+print('Build model...')
 
-I = Input(batch_shape=(batch_size, maxlen))
-x = Embedding(max_features, 128)(I)
-x = DeformableConv1D(32, 7, offset = Conv1D(32, 7, activation='relu')(x))(x)
-x = MaxPooling1D(5)(x)
-x = DeformableConv1D(32, 7, offset = Conv1D(32, 7, activation='relu')(x))(x)
+# we start off with an efficient embedding layer which maps
+# our vocab indices into embedding_dims dimensions
+I = Input(shape=(maxlen,))
+#I = Input(batch_shape=(batch_size, maxlen))
+x = Embedding(max_features, embedding_dims)(I)
+x = Dropout(0.2)(x)
+
+# we add a Convolution1D, which will learn filters
+# word group filters of size filter_length:
+x = DeformableConv1D(filters, kernel_size)(x)
+x = Activation('relu')(x)
+
+# we use max pooling:
 x = GlobalMaxPooling1D()(x)
-x = Dense(1)(x)
 
-model = models.Model(inputs=I, outputs=x)
+# We add a vanilla hidden layer:
+x = Dense(hidden_dims)(x)
+x = Dropout(0.2)(x)
+x = Activation('relu')(x)
+
+# We project onto a single unit output layer, and squash it with a sigmoid:
+x = Dense(1)(x)
+x = Activation('sigmoid')(x)
+
+model = Model(inputs=I, outputs=x)
 
 model.summary()
 
-model.compile(optimizer=RMSprop(lr=1e-4),
-                loss='binary_crossentropy',
-                metrics=['acc'])
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
 
-history = model.fit(x_train, y_train,
-                    epochs=10,
-                    batch_size=batch_size,
-                    validation_split=0.2)
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test))
 
-import matplotlib.pyplot as plt
-
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-epochs = range(1, len(acc) + 1)
-
-plt.plot(epochs, acc, 'bo', label='Training acc')
-plt.plot(epochs, val_acc, 'b', label='Validation acc')
-
-plt.title('Training and validation accuracy')
-plt.legend()
-
-plt.figure()
-
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.legend()
-
-plt.show()
